e5f8dfdea464a253c1997d0d19ce74279ccc59d1
NunezGabriel <emilio.nunez.arenas@gmail.com>
2022-04-05 17:20:47 -0500

    actualizacion de archivo

--- Source.cpp
+++ Source.cpp
@@ -1,14 +1,25 @@
 #include <iostream>
-
+using namespace std;
 int main()
 {
-	int a = 124;
-	int b = 64;
+	int a ;
+	int b ;
 	int r = 0;
 	int q = 0;
 	int x = 0, x1 = 0, x2 = 1;
 	int y = 0, y1 = 1, y2 = 0;
-
+	
+	cout<<"ingrese un valor para a: ";cin>>a;
+	cout<<"--------------------------"<<endl;
+	while(a<0){
+	cout<<"ingrese un valor POSITIVO para a: ";cin>>a;	
+	}
+	cout<<"ingrese un valor para b: ";cin>>b;
+	cout<<"--------------------------"<<endl;
+	while(b<0){
+	cout<<"ingrese un valor POSITIVO para b: ";cin>>b;	
+	}
+		
 	if (b == 0)
 	{
 		return b;
@@ -26,5 +37,5 @@ int main()
 		y2 = y1;
 		y1 = y;
 	}
-	std::cout << a << " " << x2 << " " << y2;
+	cout <<"{ "<<"mcd(a,b) = "<< a << ";" <<" x = " << x2 << ";"<<" y = " << y2<<" }";
 }
